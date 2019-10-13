d92e07170b0e8b4107b378e56c33ed7c0b6bc0ed
Liam Krieling <lkrielin@c3r7s7.wethinkcode.co.za>
2019-10-13 02:23:30 +0200

    015 complete check detail.html/php id=product box

--- Functions/functions.php
+++ Functions/functions.php
@@ -50,7 +50,7 @@ function getPro() {
 
                 <h3>$pro_title</h3>
 
-                <img src='admin_area/product_images/$pro_image' width='180' height '180' />
+                <img src='admin_area/product_images/$pro_image' width='180' height='180' />
                 
                 <p><b> $ $pro_price</b></p>
 
