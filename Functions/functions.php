a316a0d63a180c31e9b248ddad77f296552d5833
Timothy Webb <twebb@c3r7s9.wethinkcode.co.za>
2019-10-13 15:54:26 +0200

    04-003

--- Functions/functions.php
+++ Functions/functions.php
@@ -63,8 +63,8 @@ function total_items(){
         $run_items = mysqli_query($con, $get_items);
 
         $count_items = mysqli_num_rows($run_items);
-        
-        
+
+
 
     }
     else{
@@ -82,6 +82,46 @@ function total_items(){
     }
     echo $count_items;
 }
+
+//Getting total price of items in cart
+
+function total_price(){
+
+    $total = 0;
+
+    global $con;
+
+    $ip = getIp();
+
+    $sel_price = "select * from cart where ip_add='$ip'";
+
+    $run_price = mysqli_query($con, $sel_price);
+
+    while($p_price=mysqli_fetch_array($run_price)){
+
+        $pro_id = $p_price['p_id'];
+
+        $pro_price = "select * from products where product_id='$pro_id'";
+
+        $run_pro_price = mysqli_query($con, $pro_price);
+
+        while($pp_price = mysqli_fetch_array($run_pro_price)){
+
+            $product_price = array($pp_price['product_price']);
+            
+            $values = array_sum($product_price);
+
+            $total += $values;
+
+        }
+
+
+    }
+
+    echo "$".$total;
+
+}
+
 
 function getCats(){
     global $con;
