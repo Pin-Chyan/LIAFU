57be2b8240b3c48bf8a87cdc9745a187e1419a2e
Liam Krieling <lkrielin@c3r7s7.wethinkcode.co.za>
2019-10-13 12:24:37 +0200

    echo fix inside product

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -67,14 +67,14 @@ include("includes/db.php");
         $product_desc = $_POSTp['product_desc'];
         $product_keywords = $_POST['product_keywords'];
         
-
+        echo $product_title."\n";
         //getting the image from the field
         $product_image = $_FILES['product_image']['name'];
         $product_image_tmp = $_FILES['product_image']['tmp_name'];
 
         move_uploaded_file($product_image_tmp,"product_images/$product_image");
 
-        echo $insert_product = "INSERT INTO products ";
+        $insert_product = "INSERT INTO products ";
         $prod = "(product_cat, product_brand,product_title,product_price,product_desc,product_image,product_keywords) values ";
         $end = "('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
 
